24d2fb0653ca3b70d8e9411e9e915889f5478f35
Cyranixonpj <s27730@pjwstk.edu.pl>
2024-06-10 22:21:04 +0200

    start

--- ProbneKolokwium/ProbneKolokwium/Service/OrderService.cs
+++ ProbneKolokwium/ProbneKolokwium/Service/OrderService.cs
@@ -15,19 +15,20 @@ public class OrderService(DatabaseContext context): IOrderService
 {
     public async Task<GetClientOrdersResponseModel> GetOrdersByIdAsync(int id)
     {
-        // var result = await context.Clients
-        //     .Where(e => e.ClientId == id)
-        //     .Select(e => new GetClientOrdersResponseModel()
-        //     {
-        //         OrderId = e.Orders.Select(e => e.OrderId).FirstOrDefault(),
-        //         ClientsLastName = e.ClientLastName,
-        //         CreatedAt = e.Orders.Select(e => e.CreatedAt).FirstOrDefault(),
-        //         FullfilledAt = e.Orders.Select(e => e.FullfilledAt).FirstOrDefault(),
-        //         
-        //
-        //     }).FirstOrDefaultAsync();
-        //
-        //
-        // return null;
+        var result = await context.Clients
+            .Where(e => e.ClientId == id)
+            .Select(e => new GetClientOrdersResponseModel()
+            {
+                OrderId = e.Orders.Select(e => e.OrderId).FirstOrDefault(),
+                ClientsLastName = e.ClientLastName,
+                CreatedAt = e.Orders.Select(e => e.CreatedAt).FirstOrDefault(),
+                FullfilledAt = (DateTime)e.Orders.Select(e => e.FullfilledAt).FirstOrDefault(),
+                Status = e.Orders.Select(e => e.StatStausId.StatusName).FirstOrDefault(),
+                
+        
+            }).FirstOrDefaultAsync();
+        
+        
+        return null;
     }
 }
